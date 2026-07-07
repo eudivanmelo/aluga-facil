9ecc3547d6582162c2455228c88ed14cbfbb22db
eudivanmelo <eudivan44@gmail.com>
2026-07-07 12:31:47 -0300

    feat: created map component and page using maplibre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MapaComponent } from './pages/mapa/mapa.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'mapa', component: MapaComponent },
 ];
